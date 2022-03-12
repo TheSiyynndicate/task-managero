52e35c699534dd599a82c5833b24a9341f9c9053
Muhammed Malan <mohammedmalan60@gmail.com>
2022-03-12 14:38:10 +0100

    test

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Thesiyynndicate:RCIIZ8m5X7Jg7r0y@cluster0.bz5c7.mongodb.net/task-managero-api?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Thesiyynndicate:BSPPwdq#8G-DxYa@task-manager-api.bz5c7.mongodb.net/task-managero-api?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
@@ -8,6 +8,7 @@ client.connect(err => {
   client.close();
 });
 
+module.exports = client;
 // const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1:27017/task-managero-api', {
