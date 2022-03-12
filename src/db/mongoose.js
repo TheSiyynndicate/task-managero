81ce6579c4f1c5a7887269ac49b339b6a9e22da9
Muhammed Malan <mohammedmalan60@gmail.com>
2022-03-12 13:51:33 +0100

    database cconnect

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,17 @@
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Thesiyynndicate:RCIIZ8m5X7Jg7r0y@cluster0.bz5c7.mongodb.net/task-managero-api?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/task-managero-api', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// })
