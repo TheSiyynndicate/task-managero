6799060f9a7f30cea2b77f062b2aa302c53e3ab9
Muhammed Malan <mohammedmalan60@gmail.com>
2022-03-12 15:13:49 +0100

    mongo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,18 @@
-
+const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Thesiyynndicate:BSPPwdq#8G-DxYa@task-manager-api.bz5c7.mongodb.net/task-managero-api?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const uri = "mongodb+srv://Thesiyynndicate:BSPPwdq#8G-DxYa@task-manager-api.bz5c7.mongodb.net/task-manager-api?retryWrites=true&w=majority";
 
-module.exports = client;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.version });
+// client.connect(err => {
+//   const collection = client.db("task-manager-api").collection("users");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+const mongooseClient =  mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology: true, serverApi: ServerApiVersion.v1},(val)=>{});
+
+module.exports = mongooseClient;
 // const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1:27017/task-managero-api', {
