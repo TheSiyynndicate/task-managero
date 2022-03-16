e3d151c48de60025f60fbbaed3866751ff82da31
Muhammed Malan <mohammedmalan60@gmail.com>
2022-03-16 12:48:45 +0100

    Json fixes

--- src/routers/user.js
+++ src/routers/user.js
@@ -57,13 +57,20 @@ router.post("/users/logoutAll", auth, async (req, res) => {
 });
 
 // Create user
-router.post("/users", async (req, res) => {
+router.post("/users/create", async (req, res) => {
+    //user, token
   try {
     const user = new User(req.body);
     const token = await user.generateWebToken();
 
     // Sends the user and the generated token only
-    res.status(201).send({ user, token });
+    res.status(201).json({ 
+        "message":"User created successfully",
+        "data": {
+            "user":user,
+            "token":token
+        }
+     });
   } catch (e) {
     res.status(400).send(e);
   }
