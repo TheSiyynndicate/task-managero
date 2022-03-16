da7162be8e949d5d42cbb54a95201f40a8144508
Muhammed Malan <mohammedmalan60@gmail.com>
2022-03-16 15:25:21 +0100

    Json fix

--- src/routers/user.js
+++ src/routers/user.js
@@ -58,19 +58,19 @@ router.post("/users/logoutAll", auth, async (req, res) => {
 
 // Create user
 router.post("/users/create", async (req, res) => {
-    //user, token
+  //user, token
   try {
     const user = new User(req.body);
     const token = await user.generateWebToken();
 
     // Sends the user and the generated token only
-    res.status(201).json({ 
-        "message":"User created successfully",
-        "data": {
-            "user":user,
-            "token":token
-        }
-     });
+    res.status(201).json({
+      message: "User created successfully",
+      data: {
+        user: user,
+        token: token,
+      },
+    });
   } catch (e) {
     res.status(400).send(e);
   }
